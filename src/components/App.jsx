507c51eb5770df02d5d135dc283815bbb6742bf4
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2023-01-19 10:05:53 +0100

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ContactList from 'components/ContactList/ContactList';
 import AddingContacts from 'components/AddingContacts/AddingContacts';
-import { getFromStorage } from 'service/d';
-import { saveToStorage } from 'service/d';
+import { getFromStorage } from 'service/localStorageServic';
+import { saveToStorage } from 'service/localStorageServic';
 
 export const App = () => {
   const [contacts, setContacts] = useState(getFromStorage('contacts'));
